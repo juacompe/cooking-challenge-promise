27a5370d640cd94ba416219e3e4745f7374100ee
Weera Kasetsin <wkasetsin@gmail.com>
2014-06-19 08:05:41 +0700

    Design promise chaining

--- app.js
+++ app.js
@@ -6,8 +6,23 @@ app.controller('CookingChallengeController', function($scope, $q) {
   var ACHIO = 'achio';
   var FIRST_CUE = "north moutain in Shitusoka";
   var SECOND_CUE = "waterfall in Kanangawa";
-  var THIRD_CUE = "blue sea in Hokido"; 
+  var THIRD_CUE = "blue sea in Hokido";
 
   $scope.winner = "";
   $scope.loser = "";
+
+  findIngredientBy = function(challengerName) {
+    var deferred = $q.defer();
+
+    useFirstCue(challengerName)
+      .then(useSecondCue)
+      .then(useThirdCue)
+      .then(function(result) {
+
+        }, function(reason) {
+
+        });
+        
+    return deferred.promise;
+  };
 });
