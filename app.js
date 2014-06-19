f1b7d6a45ce3ec3eb0158baf4fbfc3e9fa202cf1
Weera Kasetsin <wkasetsin@gmail.com>
2014-06-19 08:03:43 +0700

    Define constant variable to use in app

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var app = angular.module('app', []);
 
 app.controller('CookingChallengeController', function($scope, $q) {
-  
+  var RARE_INGREDIENT = 'EARTH';
+  var ICHIKO = 'ichiko';
+  var ACHIO = 'achio';
+  var FIRST_CUE = "north moutain in Shitusoka";
+  var SECOND_CUE = "waterfall in Kanangawa";
+  var THIRD_CUE = "blue sea in Hokido"; 
+
+  $scope.winner = "";
+  $scope.loser = "";
 });
