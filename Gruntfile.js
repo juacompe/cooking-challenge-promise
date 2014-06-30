172e574728cefb24c955e9d14bb30ea740fbac31
juacompe <juacompe@gmail.com>
2014-06-30 13:10:45 +0700

    run test using grunt > karma > jasmine

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        karma: karmaConfig(),
+        jshint: jshintConfig(),
+    });
+
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    grunt.registerTask('default', ['jshint', 'karma:ci']);
+};
+
+function karmaConfig() {
+    var config = {};
+    config.options = {};
+    config.options.configFile = 'karma.conf.js';
+    config.ci = {};
+    config.ci.singleRun = true; 
+    config.dev = {}; 
+    config.dev.reporters = 'dots'; 
+    return config;
+}
+
+function jshintConfig() {
+    var jshint;
+    jshint = {};
+    jshint.code = ['app2.js', 'Gruntfile.js'];
+    jshint.spec = {};
+    jshint.spec.files = {};
+    jshint.spec.files.src = ['specs/**/*.js'];
+    jshint.spec.options = {};
+    jshint.spec.options.globals = {};
+    jshint.spec.options.globals.describe = true;
+    jshint.spec.options.globals.it = true;
+    jshint.spec.options.globals.expect = true;
+    return jshint;
+}
---
+++ karma.conf.js
@@ -0,0 +1,23 @@
+module.exports = function(karma) {
+    var config = {};
+    config.basePath = '';
+    config.frameworks = ['jasmine'];
+    config.files = [
+        'angular.js',
+        'app2.js',
+        'specs/**',
+    ];
+    config.reporters = ['dots'];
+    config.port = 9876;
+    config.runnerPort = 9100;
+    config.colors = true;
+    config.logLevel = karma.LOG_INFO;
+    config.autoWatch = true;
+    config.browsers = ['Chrome'];
+    config.captureTimeout = 60000;
+    config.singleRun = false;
+
+    karma.set(config);
+};
+
+
